eb20f165867b44fe3602d6b52e916b40456d8707
Luis Verdugo <maxesquerra@gmail.com>
2026-06-06 15:52:32 -0600

    fix port for Railway

--- app.py
+++ app.py
@@ -124,4 +124,5 @@ def download_report(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
